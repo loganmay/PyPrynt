fcf014e3c35246a9e1c342ffdccc917041631cce
Logan May <logsta.may@gmail.com>
2016-11-30 23:38:58 -0500

    Nicole and Rob's awesomeness

--- library.py
+++ library.py
@@ -5,18 +5,50 @@
 # Imports
 from Tkinter import *
 from collections import Counter
+import os
 #import numpy
-
+f = open('PrinterReport.txt', 'w')
 
 # # # # # Pages Methods # # # # #
+#### RUN BUTTON ####
+def runReport(location, date, time, pages, cost, quota, maxPagesVar,
+              minPagesVar, avgPagesVar, largeSmallVar, listLocationVar,
+              printerUsersVar, perPrinterUsersVar, mostCostlyVar, printerCostVar,
+              mostUsedDateVar, leastUsedDateVar, mostUsedTimeVar, leastUsedTimeVar):
+
+    if maxPagesVar == 1:
+        calcMaxPages(pages)
+    if minPagesVar == 1:
+        calcMinPages(pages)
+    if avgPagesVar == 1:
+        printAvg(pages)
+    if largeSmallVar == 1:
+        leastandMostLocation(location)
+    if listLocationVar == 1:
+        printListofRooms(location)
+    if printerUsersVar == 1:
+        printerUser(quota)
+    if perPrinterUsersVar == 1:
+        perPrinterUser(location, quota)
+    if mostCostlyVar == 1:
+        mostCostly(location, cost)
+    if printerCostVar == 1:
+        totalCost(location, cost)
+    if mostUsedDateVar == 1:
+        mostUsedDate(date)
+    if leastUsedDateVar == 1:
+        leastUsedDate(date)
+    if mostUsedTimeVar == 1:
+        mostUsedTime(date)
+    if leastUsedTimeVar == 1:
+        leastUsedTime(date)
+    os.startfile('PrinterReport.txt')
+    f.close()
+#####END RUN BUTTON#######
 
 
 # Calculate the max pages in a single print
 def calcMaxPages(pages):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Max Pages")
-    branch.geometry("250x100")
     max = 0
     for e in pages:
         if e > max:
@@ -24,20 +56,11 @@ def calcMaxPages(pages):
 
     # This is the output message
     message = "Max Pages: " + str(max)
-
-    # Print output in new window using a label
-    branchLabel = Label(branch, text=message)
-    branchLabel.pack(side=TOP)
-
-    branch.mainloop()
+    print >>f, message
+    f.write(message)
 
 # Calculate the min pages in a single print
 def calcMinPages(pages):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Min Pages")
-    branch.geometry("250x100")
-
     min = max
     for e in pages:
         if e < min:
@@ -45,12 +68,8 @@ def calcMinPages(pages):
 
     # This is the ouput message
     message = "Minimum Pages: " + str(min)
+    print >>f, message
 
-    # Print output in new window using a label
-    branchLabel = Label(branch, text=message)
-    branchLabel.pack(side=TOP)
-
-    branch.mainloop()
 # Calculate the average number of pages in all the prints
 def calcAvgPages(pages):
 
@@ -59,30 +78,21 @@ def calcAvgPages(pages):
     average = totalPages/numEntries
     return average
 
-
 # Print the average
 def printAvg(pages):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Average Pages")
-    branch.geometry("250x100")
 
     # Call calcAvgPages function to get average pages printer
     average = calcAvgPages(pages)
     # Output Message
     message = "Average number of pages printed: " + str(average)
     # Print output in new window using a label
-    branchLabel = Label(branch, text=message)
-    branchLabel.pack(side=TOP)
-
-    branch.mainloop()
+    print >>f, message
 
 # Calculate the standard deviation of the number of pages in the prints
 def calcStdPages(pages):
 
     stddev = numpy.std(pages)
     return stddev
-
 
 # Returns the index of the biggest print
 def indexOfMaxPages(pages):
@@ -91,9 +101,7 @@ def indexOfMaxPages(pages):
         if e == calcMaxPages(pages):
             return i
 
-
 # # # # # End Pages Methods # # #
-
 
 # # # # # Location Methods # # # # #
 
@@ -105,64 +113,34 @@ def orderLocations(location):
     locationAscending = sorted(locCounter.items(), key=itemgetter(1), reverse=True)
     return locationAscending
 
-
 # Print the most and least used locations
 def leastandMostLocation(location):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Location Info")
-    branch.geometry("300x100")
-
     # Get organzied list
     locationAscending = orderLocations(location)
 
     # Print Most Used Location in the new window using a label
     mostUsed = "Most used Room: " + locationAscending[0][0]
-    mostUsedLabel = Label(branch, text=mostUsed)
-    mostUsedLabel.pack(side=TOP)
+    print >>f, mostUsed
 
-    # Print Least Used Location in the new windo using a label
+    # Print Least Used Location in the new window using a label
     leastUsed = "Leased used Room: " + locationAscending[len(locationAscending) - 1][0]
-    leastUsedLabel = Label(branch, text=leastUsed)
-    leastUsedLabel.pack(side=TOP)
-
-    branch.mainloop()
+    print >>f, leastUsed
 
 # Print the list of rooms in ascending order according to number of prints
 def printListofRooms(location):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Location Info")
-    branch.geometry("300x250")
-
     # Get organized list
     locationAscending = orderLocations(location)
-    listLabel = Label(branch, text="List of Rooms: ")
-    listLabel.pack(side=TOP)
 
     # Print the list to the new window my recalling a label and incrementing its position using the grid
     for key, value in locationAscending:
         message = "{} || {} prints".format(key, value)
-        listLabel = Label(branch, text=message)
-        listLabel.pack(side=TOP)
-
-    branch.mainloop()
-
-
+        print >>f, message
 # # # # # End Location Methods # # #
-
 
 # # # # # Quota Methods # # # # #
 
 # See if more students or professors use the printers
 def printerUser(quota):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Printer Users")
-    branch.geometry("250x100")
-
-
-
     # Initialize counts and output message
     printerUserMessage = ""
     prof = 0
@@ -180,18 +158,10 @@ def printerUser(quota):
         printerUserMessage = "More Students use the printers."
 
     # Print output in the new window using a label
-    userLabel = Label(branch, text=printerUserMessage)
-    userLabel.pack(side=TOP)
-
-    branch.mainloop()
+    print >>f, printerUserMessage
 
 # Look at professors vs. students for each printer individually
 def perPrinterUser(location, quota):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Printer User List")
-    branch.geometry("350x250")
-
     # Initialize variables
     printer = ["HPLaserJet 600 Insalaco Lab RM 20", "HPLaserJet 600 Library Left Printer", "HPLaserJet 600 Library Right Printer", "HPLaserJet 600 Passan Hall Lounge","HPLaserJet 600 Science RM 205","HPLaserJet 600 Mercy Hall RM335","HPLaserJet 600 Annex Passan Hall Lab","HPLaserJet 600 Henry Lounge","HPLaserJet 600 Mercy Hall RM349","HP Color LaserJet M651 Library"]
     perPrinterMessage = ""
@@ -214,8 +184,7 @@ def perPrinterUser(location, quota):
         if prof > stud:
             # Output message in new window using a label
             perPrinterMessage = "More Faculty use printer " + printer[thisOtherIndex]
-            perPrinterLabel = Label(branch, text=perPrinterMessage)
-            perPrinterLabel.grid(row=thisOtherIndex, sticky=W)
+            print >>f, perPrinterMessage
 
             #print "Prof = ", prof, " Stud = ", stud
             prof = 0
@@ -225,8 +194,7 @@ def perPrinterUser(location, quota):
         elif prof < stud:
             # Output message in new window using a label
             perPrinterMessage = "More students use printer " + printer[thisOtherIndex]
-            perPrinterLabel = Label(branch, text=perPrinterMessage)
-            perPrinterLabel.grid(row=thisOtherIndex, sticky=W)
+            print >>f, perPrinterMessage
 
            # print "Prof = ", prof, " Stud = ", stud
             prof = 0
@@ -236,23 +204,15 @@ def perPrinterUser(location, quota):
         elif prof == stud:
             # Output message in new window using a label
             perPrinterMessage = "Students and Professors use printer " + printer[thisOtherIndex] + " equally"
-            perPrinterLabel = Label(branch, text=perPrinterMessage)
-            perPrinterLabel.grid(row=thisOtherIndex, sticky=W)
+            print >>f, perPrinterMessage
 
             #print "Prof = ", prof, " Stud = ", stud
             prof = 0
             stud = 0
         thisOtherIndex += 1
 
-    branch.mainloop()
-
 # Find the most costly printer and its cost
 def mostCostly(location, cost):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Most Costly Printer")
-    branch.geometry("375x100")
-
     # Initialize variables
     maxCost = 0
     currentCost = 0
@@ -286,33 +246,21 @@ def mostCostly(location, cost):
     # Message concerning the most costly printer
     mostCostlyPrinter = "The printer that is most costly:  " + profitPrinter
     # Message concerning the total most costly
-    mostCostly= "With a total cost of: " + str(maxCost)
+    mostCostlymessage = "With a total cost of: " + str(maxCost)
 
-    # Print most costly printer into new window using a label
-    mostCostlyPrinterLabel = Label(branch, text=mostCostlyPrinter)
-    mostCostlyPrinterLabel.grid(row=0, sticky=W)
-
-    # Print the most costly total into the same window using a different label
-    mostCostlyLabel = Label(branch, text=mostCostly)
-    mostCostlyLabel.grid(row=1,sticky=W)
-
-    branch.mainloop()
-
-
+    # Print most costly printer
+    print >>f, mostCostlyPrinter
+    print >>f, mostCostlymessage
 
 # Find total cost for each printer
 def totalCost(location, cost):
-    # Create a new window for Output
-    branch = Tk()
-    branch.title("Printer Total Cost List")
-    branch.geometry("350x250")
-
     # Initialize printer list
     printer = ["HPLaserJet 600 Insalaco Lab RM 20", "HPLaserJet 600 Library Left Printer",
                "HPLaserJet 600 Library Right Printer", "HPLaserJet 600 Passan Hall Lounge",
                "HPLaserJet 600 Science RM 205","HPLaserJet 600 Mercy Hall RM335",
                "HPLaserJet 600 Annex Passan Hall Lab","HPLaserJet 600 Henry Lounge",
                "HPLaserJet 600 Mercy Hall RM349","HP Color LaserJet M651 Library"]
+
     #search through each printer
     for p in printer:
         e = 0 #reset e to zero
@@ -326,10 +274,8 @@ def totalCost(location, cost):
         totalCostMessage = "Printer: {:20}  ||  Total Cost: {}".format(p, total)
 
         # Print output into the new window using a label
-        CostLabel = Label(branch, text=totalCostMessage)
-        CostLabel.grid(row=printer.index(p), sticky=W)
+        print >>f, totalCostMessage
 
-    branch.mainloop()
 # # # # # End Quota Methods # # #
 
 # # # # # Search Methods # # #
@@ -428,8 +374,225 @@ def pagesSearch(location, pages, date, value, radioValue):
         totalLabel.pack(side=TOP)
 
     branch.mainloop()
-# # # # # High Volume Methods # # # # #
 
+# # # # # Date methods # # #
+def mostUsedDate(date):
+    # Initialize pure lists
+    pureDate = []
+    pureTime = []
+
+    # Initialize exact days
+    diffDays = []
+
+    # Initialize most used day and current most used day
+    mostUsedDay = ""
+    currMost = 0
+
+    for e in range(len(date)-1):
+        currentDate = date[e].split(" ")
+        currentDate.pop(0)
+        pureDate.append(currentDate[0])
+        pureTime.append(currentDate[1])
+
+    for e in pureDate:
+        if e in diffDays:
+            continue
+        else:
+            diffDays.append(e)
+    # for e in diffDays: pagesList.insert(END, e)
+    for day in diffDays:
+
+        e = 0
+        dayUsageTotal = 0   # dayUsageTotal to find the most used day
+
+        while e < len(pureDate):
+            if pureDate[e] == day:
+                dayUsageTotal += 1
+            e += 1
+
+        if dayUsageTotal > currMost:
+            currMost = dayUsageTotal
+            mostUsedDay = day
+
+        #print "On {}, {} jobs were printed".format(day, dayUsageTotal)
+    dateMessage = "The most used date was {}\n With {} total jobs printed that day".format(mostUsedDay, currMost)
+    print >>f, dateMessage
+
+def leastUsedDate(date):
+    # Initialize pure lists
+    pureDate = []
+    pureTime = []
+
+    # Initialize exact days
+    diffDays = []
+
+    # Initialize most used dayad current most used day
+    leastUsedDay = ""
+    currLeast = 10000
+
+    for e in range(len(date) - 1):
+        currentDate = date[e].split(" ")
+        currentDate.pop(0)
+        pureDate.append(currentDate[0])
+        pureTime.append(currentDate[1])
+
+    for e in pureDate:
+        if e in diffDays:
+            continue
+        else:
+            diffDays.append(e)
+
+    # for e in diffDays: pagesList.insert(END, e)
+    for day in diffDays:
+
+        e = 0
+        dayUsageTotal = 0  # dayUsageTotal to find the most used day
+
+        while e < len(pureDate):
+            if pureDate[e] == day:
+                dayUsageTotal += 1
+            e += 1
+
+        if dayUsageTotal < currLeast:
+            currLeast = dayUsageTotal
+            leastUsedDay = day
+
+        print >>f, "On {}, {} jobs were printed".format(day, dayUsageTotal)
+
+    dateMessage = "The least used date was {}\n With {} total jobs printed that day".format(leastUsedDay, currLeast)
+    print >>f, dateMessage
+
+def mostUsedTime(date):
+    # Initialize pure lists
+    pureDate = []
+    pureTime = []
+    pureHour = []
+
+    # Initialize exact days
+    diffHours = []
+
+    mostUsedHour = ""
+    currMost = 0
+
+    # Split the date into pure forms
+    for e in range(len(date) - 1):
+        currentDate = date[e].split(" ")
+        currentDate.pop(0)
+        pureDate.append(currentDate[0])
+        pureTime.append(currentDate[1])
+
+    # Split the pureTime into pureHours
+    for e in range(len(pureTime) - 1):
+        currentTime = pureTime[e].split(":")
+        pureHour.append(currentTime[0])
+
+    # Make sure each hour is only counted once
+    for e in pureHour:
+        if e in diffHours:
+            continue
+        else:
+            diffHours.append(e)
+
+    # for e in diffHours: pagesList.insert(END, e)
+    for hour in diffHours:
+
+        e = 0
+        hourUsageTotal = 0   # hourUsageTotal to find the most used hour
+
+        while e < len(pureHour):
+            if pureHour[e] == hour:
+                hourUsageTotal += 1
+            e += 1
+
+        if hourUsageTotal > currMost:
+            currMost = hourUsageTotal
+            mostUsedHour = hour
+
+        #print >>f, "At {}, {} jobs were printed".format(hour, hourUsageTotal)
+
+    # if selection statement to make it easier for the user to read
+    if mostUsedHour == "00":
+        mostUsedHour = "12am"
+    elif int(mostUsedHour) < 12:
+        mostUsedHour += "am"
+    else:
+        regularTime = int(mostUsedHour) - 12
+        mostUsedHour = str(regularTime) + "pm"
+
+    hourMessage = "The most used hour was {}\n With {} total jobs printed during that hour".format(mostUsedHour,
+                                                                                                   currMost)
+    print >>f, hourMessage
+
+def leastUsedTime(date):
+    # Initialize pure lists
+    pureDate = []
+    pureTime = []
+    pureHour = []
+
+    # Initialize exact days
+    diffHours = []
+
+    mostUsedHour = ""
+    currMost = 100000
+
+    # Split the date into pure forms
+    for e in range(len(date) - 1):
+        currentDate = date[e].split(" ")
+        currentDate.pop(0)
+        pureDate.append(currentDate[0])
+        pureTime.append(currentDate[1])
+
+    # Split the pureTime into pureHours
+    for e in range(len(pureTime) - 1):
+        currentTime = pureTime[e].split(":")
+        pureHour.append(currentTime[0])
+
+    # Make sure each hour is only counted once
+    for e in pureHour:
+        if e in diffHours:
+            continue
+        else:
+            diffHours.append(e)
+
+    # for e in diffHours: pagesList.insert(END, e)
+    for hour in diffHours:
+
+        e = 0
+        hourUsageTotal = 0  # hourUsageTotal to find the most used hour
+
+        while e < len(pureHour):
+            if pureHour[e] == hour:
+                hourUsageTotal += 1
+            e += 1
+
+        if hourUsageTotal < currMost:
+            currMost = hourUsageTotal
+            mostUsedHour = hour
+
+        #print >>f, "At {}, {} jobs were printed".format(hour, hourUsageTotal)
+
+    # if selection statement to make it easier for the user to read
+    if mostUsedHour == "00":
+        mostUsedHour = "12am"
+    elif int(mostUsedHour) < 12:
+        mostUsedHour = str(int(mostUsedHour)) + "am"
+    else:
+        regularTime = int(mostUsedHour) - 12
+        mostUsedHour = str(regularTime) + "pm"
+
+    hourMessage = "The least used hour was {}\n With {} total jobs printed during that hour".format(mostUsedHour,
+                                                                                                   currMost)
+    print >>f, hourMessage
+
+
+
+
+
+
+
+
+
+# # # # # High Volume Methods # # # # #
 # Returns a list containing the indices of high volume prints in an array
 #  (# prints > or = 1 std dev above the mean)
 def highVolumeIndices(pages):
